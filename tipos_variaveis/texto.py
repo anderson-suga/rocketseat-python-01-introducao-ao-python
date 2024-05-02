c7a9825f3127710147d27646938651a3e63b593e
Anderson Suga <anderson.suga@gmail.com>
2024-05-02 14:08:25 -0300

    Texto - parte 1

---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,10 @@
+# Declaração
+nome_completo = "Gabriel Casemiro"
+
+nome_completo_aspas = """Gabriel Casemiro"""
+
+nome_completo_quebra = "Gabriel \
+Casemiro"
+
+nome = "Gabriel"
+sobrenome = "Casemiro"
