a03415985e56415bf03f5914ab94bc66ff0cca35
Anderson Suga <anderson.suga@gmail.com>
2024-05-02 17:48:37 -0300

    Loop While

---
+++ while.py
@@ -0,0 +1,10 @@
+print("Exemplo de while")
+contador = 0
+while True:
+    print("Contagem:", contador)
+    contador += 1
+    if contador == 5:
+        print("Saindo pelo break")
+        break
+
+print("Programa finalizado")
